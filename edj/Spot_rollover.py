1d53822d1742928cb6e37e5cf5156122dea6bc58
tigakub <tigakub@mac.com>
2021-07-13 14:02:23 -0700

    edj: rollover script

---
+++ edj/Spot_rollover.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+from Spot import *
+import time
+
+if __name__ == '__main__':
+    spot = Spot()
+
+    # It's ALIVE!
+    spot.power_on()
+
+    # Strike some poses
+    spot.stand()
+    time.sleep(5.0)
+
+    spot.belly_rub(direction=BELLY_RUB_RIGHT)
+    time.sleep(10.0)
+
+    # Power down
+    spot.estop(graceful=True)
+
+    print('Trying to make Python GC the Spot object')
+    spot = None
+    time.sleep(5.0)
+
+    exit(0)
