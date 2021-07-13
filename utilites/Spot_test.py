ed23e3b67dfcbc8a2c8fda5534e6702ffa06f69e
tigakub <tigakub@mac.com>
2021-07-13 12:27:50 -0700

    Minor typo

--- utilites/Spot_test.py
+++ utilites/Spot_test.py
@@ -5,22 +5,21 @@ from Spot import *
 if __name__ == '__main__':
     spot = Spot()
 
-	# It's ALIVE!
-	spot.power_on()
+    # It's ALIVE!
+    spot.power_on()
 
-	# Strike some poses
-	spot.stand()
-	time.sleep(5.0)
+    # Strike some poses
+    spot.stand()
+    time.sleep(5.0)
 
-	spot.belly_rub(direction=BELLY_RUB_RIGHT)
-	time.sleep(5.0)
+    spot.belly_rub(direction=BELLY_RUB_RIGHT)
+    time.sleep(5.0)
 
-	spot.self_right()
-	time.sleep(5.0)
+    spot.self_right()
+    time.sleep(5.0)
 
-	spot.stand()
-	time.sleep(5.0)
+    spot.stand()
+    time.sleep(5.0)
 
-	# Power down
-	time.estop(graceful=True)
-    
+    # Power down
+    time.estop(graceful=True)
