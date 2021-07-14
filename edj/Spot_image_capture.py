21435406e13538bb816a57f33c38d86fdccda691
tigakub <tigakub@mac.com>
2021-07-13 19:07:25 -0700

    edj: updating lease keep-alive handling

--- edj/Spot_image_capture.py
+++ edj/Spot_image_capture.py
@@ -10,34 +10,37 @@ import cv2
 if __name__ == '__main__':
     spot = Spot()
 
-    list = ['right_fisheye_image', 'right_depth_in_visual_frame', 'left_fisheye_image', 'left_depth', 'frontright_fisheye_image', 'frontright_depth', 'frontleft_fisheye_image', 'frontleft_depth', 'back_fisheye_image', 'back_depth']
-    images = spot.get_images(list)
-    image_dict = dict(zip(list, images))
+    try:
+        list = ['right_fisheye_image', 'right_depth_in_visual_frame', 'left_fisheye_image', 'left_depth', 'frontright_fisheye_image', 'frontright_depth', 'frontleft_fisheye_image', 'frontleft_depth', 'back_fisheye_image', 'back_depth']
+        images = spot.get_images(list)
+        image_dict = dict(zip(list, images))
 
-    for source, image_result in image_dict.items():
-        print(f'From source: {source}')
-        image = image_result.shot.image
-        print(f'\twidth: {image.cols}')
-        print(f'\theight: {image.rows}')
-        if image.pixel_format == image_pb2.Image.PIXEL_FORMAT_DEPTH_U16:
-            print('\tformat: depth_16')
-            dtype = np.uint16
-        else:
-            print('\tformat: rgb_8')
-            dtype = np.uint8
+        for source, image_result in image_dict.items():
+            print(f'From source: {source}')
+            image = image_result.shot.image
+            print(f'\twidth: {image.cols}')
+            print(f'\theight: {image.rows}')
+            if image.pixel_format == image_pb2.Image.PIXEL_FORMAT_DEPTH_U16:
+                print('\tformat: depth_16')
+                dtype = np.uint16
+            else:
+                print('\tformat: rgb_8')
+                dtype = np.uint8
 
-        img = np.fromstring(image.data, dtype=dtype)
-        print(img)
-        if image.pixel_format == image_pb2.Image.FORMAT_RAW:
-            img = img.reshape(image.rows, image.cols)
-        elif image.pixel_format == image_pb2.Image.PIXEL_FORMAT_DEPTH_U16:
-            img = cv2.imdecode(img, cv2.IMREAD_ANYDEPTH)
-        else:
-            img = cv2.imdecode(img, cv2.IMREAD_ANYCOLOR)
+            img = np.fromstring(image.data, dtype=dtype)
+            print(img)
+            if image.pixel_format == image_pb2.Image.FORMAT_RAW:
+                img = img.reshape(image.rows, image.cols)
+            elif image.pixel_format == image_pb2.Image.PIXEL_FORMAT_DEPTH_U16:
+                img = cv2.imdecode(img, cv2.IMREAD_ANYDEPTH)
+            else:
+                img = cv2.imdecode(img, cv2.IMREAD_ANYCOLOR)
 
-        print(img)
-        filename = f'{source}.png'
-        cv2.imwrite(filename, img)
+            print(img)
+            filename = f'{source}.png'
+            cv2.imwrite(filename, img)
+    except:
+        print('Exception')
 
     print('Trying to make Python GC the Spot object')
     spot = None
