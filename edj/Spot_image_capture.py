8587fa6efef22adfc44ff20d738748355cd573af
tigakub <tigakub@mac.com>
2021-07-13 18:42:16 -0700

    edj: updating lease keep-alive handling

--- edj/Spot_image_capture.py
+++ edj/Spot_image_capture.py
@@ -32,11 +32,9 @@ if __name__ == '__main__':
         else:
             img = cv2.imdecode(img, -1)
 
-        if img.data == None:
-            print('Failed to save empty image')
-        else:
-            filename = f'{source}.png'
-            cv2.imwrite(filename, img)
+        print(img)
+        filename = f'{source}.png'
+        cv2.imwrite(filename, img)
 
     print('Trying to make Python GC the Spot object')
     spot = None
