093fd66a8a6bd726044a81ea7208f9495df90b2b
tigakub <tigakub@mac.com>
2021-07-13 19:04:49 -0700

    edj: updating lease keep-alive handling

--- edj/Spot_list_image_sources.py
+++ edj/Spot_list_image_sources.py
@@ -6,13 +6,7 @@ import time
 if __name__ == '__main__':
     spot = Spot()
 
-    # It's ALIVE!
-    spot.power_on()
-
     print(f'Image sources: \n{spot.list_image_sources()}')
-
-    # Power down
-    spot.estop(graceful=True)
 
     print('Trying to make Python GC the Spot object')
     spot = None
