3736f7627cb16cc0fa490fc0a21f4dd504cd0262
tigakub <tigakub@mac.com>
2021-07-13 18:11:04 -0700

    edj: updating lease keep-alive handling

--- edj/Spot_image_capture.py
+++ edj/Spot_image_capture.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
 from Spot import *
+
+from bosdyn.api import image_pb2
+
 import time
 
 if __name__ == '__main__':
@@ -9,6 +12,14 @@ if __name__ == '__main__':
     list = ['right_fisheye_image', 'right_depth', 'left_fisheye_image', 'left_depth', 'frontright_fisheye_image', 'frontright_depth', 'frontleft_fisheye_image', 'frontleft_depth', 'back_fisheye_image', 'back_depth']
     images = spot.get_images(list)
     image_dict = dict(zip(list, images))
+
+    for source, image_result in image_dict.items():
+        print(f'From source: {source}')
+        image = image_result.shot.image
+        if image.pixel_format == image_pb2.Image.PIXEL_FORMAT_DEPTH_U16:
+            print('\tdepth_16')
+        elif image.pixel_format == image_pb2.Image.FORMAT_RAW:
+            print('\traw_8')
 
     print('Trying to make Python GC the Spot object')
     spot = None
