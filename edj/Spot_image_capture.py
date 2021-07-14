41085e5f122350efdf2e3b7246dc81d8ef80ca2e
tigakub <tigakub@mac.com>
2021-07-13 21:24:04 -0700

    edj: omg python is such a bitch

--- edj/Spot_image_capture.py
+++ edj/Spot_image_capture.py
@@ -15,10 +15,13 @@ if __name__ == '__main__':
         list = ['right_fisheye_image', 'right_depth_in_visual_frame', 'left_fisheye_image', 'left_depth_in_visual_frame', 'frontright_fisheye_image', 'frontright_depth_in_visual_frame', 'frontleft_fisheye_image', 'frontleft_depth_in_visual_frame', 'back_fisheye_image', 'back_depth_in_visual_frame']
         images = spot.get_images(list)
         image_dict = dict(zip(list, images))
-        frontright_img = None
-        frontleft_img = None
+        frontright_fisheye_img = None
+        frontleft_fisheye_img = None
+        frontright_depth_img = None
+        frontleft_depth_img = None
         stitched = None
         for source, image_result in image_dict.items():
+            print('--------------------------')
             print(f'From source: {source}')
             image = image_result.shot.image
             print(f'\twidth: {image.cols}')
@@ -61,21 +64,36 @@ if __name__ == '__main__':
             if source[0:5] == 'front':
                 print('Rotating front image')
                 img = cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE)
-                if source[6:11] == 'right':
-                    frontleft_img = img
-                if source[6:10] == 'left':
-                    frontright_img = img
-                if frontleft_img != None and frontright_img != None:
+                if source[5:18] == 'right_fisheye':
+                    frontleft_fisheye_img = img
+                if source[5:17] == 'left_fisheye':
+                    frontright_fisheye_img = img
+                if frontleft_fisheye_img != None and frontright_fisheye_img != None:
                     stitcher = cv2.createStitcher() if imutils.is_cv3() else cv2.Stitcher_create()
-                    (status, stitched) = stitcher.stitch([frontleft_img, frontright_img])
+                    (status, stitched) = stitcher.stitch([frontleft_fisheye_img, frontright_fisheye_img])
                     if status == 0:
-                        filename = f'front_stitched{extension}'
+                        filename = f'front_fisheye_stitched.jpg'
                         try:
                             cv2.imwrite(filename, stitched)
                         except:
                             print(f'Failed to write {filename}')
                     else:
-                        print('Failed to stitch front images')
+                        print('Failed to stitch front fisheye images')
+                if source[5:16] == 'right_depth':
+                    frontleft_depth_img = img
+                if source[5:15] == 'left_depth':
+                    frontright_depth_img = img
+                if frontleft_depth_img != None and frontright_depth_img != None:
+                    stitcher = cv2.createStitcher() if imutils.is_cv3() else cv2.Stitcher_create()
+                    (status, stitched) = stitcher.stitch([frontleft_depth_img, frontright_depth_img])
+                    if status == 0:
+                        filename = f'front_depth_stitched.jpg'
+                        try:
+                            cv2.imwrite(filename, stitched)
+                        except:
+                            print(f'Failed to write {filename}')
+                    else:
+                        print('Failed to stitch front depth images')
 
 
             elif source[0:5] == 'right':
