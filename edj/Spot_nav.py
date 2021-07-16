3eef2fe468e5c0aefc5611e4841ef0c091c6ea8d
tigakub <tigakub@mac.com>
2021-07-16 12:12:04 -0700

    edj: addedn Spot_nav.py test script

---
+++ edj/Spot_nav.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+from Spot import *
+
+if __name__ == '__main__':
+	spot = Spot()
+
+	try:
+		path_to_map = 'my_map_path'
+		waypoint_id_or_short_code = 'my_waypoint_id'
+
+		spot.clear_map()
+
+		spot.upload_map(my_map_path)
+
+		unique_id = spot.find_unique_waypoint_id(waypoint_id_or_short_code)
+
+		spot.threaded_nav_to(unique_id)
+
+		spot.wait_nav_thread()
+
+		status = spot.report_nav_status()
+
+	except Exception as e:
+		print('There was an exception: {e}')
+
+	# Force garbage collection
+	spot = None
+	time.sleep(5.0)
